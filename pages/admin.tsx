0fa2f03765349cd58b292bd75b9e05733dd4f374
jonkarrer <karrerjon@gmail.com>
2021-03-16 16:16:40 -0500

    origin main

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,9 +1,9 @@
-import styles from '../styles/Admin.module.css';
-import Head from 'next/head';
-import { useState } from 'react';
-import Router from 'next/router';
+import styles from "../styles/Admin.module.css";
+import Head from "next/head";
+import { useState } from "react";
+import Router from "next/router";
 
-import prisma from '../lib/prisma';
+import prisma from "../lib/prisma";
 
 export const getServerSideProps = async () => {
   const data = await prisma.post.findMany();
@@ -14,28 +14,28 @@ export const getServerSideProps = async () => {
 };
 
 async function destroy(id: number): Promise<void> {
-  await fetch(`http://localhost:3000/api/post/${id}`, {
-    method: 'DELETE',
+  await fetch(`/api/post/${id}`, {
+    method: "DELETE",
   });
-  await Router.push('/admin');
+  await Router.push("/admin");
 }
 
 function Admin({ allPosts }): JSX.Element {
-  const [catagory, setCatagory] = useState('');
-  const [title, setTitle] = useState('');
-  const [summary, setSummary] = useState('');
-  const [body, setBody] = useState('');
+  const [catagory, setCatagory] = useState("");
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [body, setBody] = useState("");
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
       const data = { catagory, title, summary, body };
-      await fetch(`http://localhost:3000/api/post`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      await fetch(`/api/post`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
       });
-      await Router.push('/admin');
+      await Router.push("/admin");
     } catch (error) {
       console.error(error);
     }
@@ -94,7 +94,7 @@ function Admin({ allPosts }): JSX.Element {
             <ul>
               {allPosts.map((item) => (
                 <li>
-                  {item.title}, {item.id},{' '}
+                  {item.title}, {item.id},{" "}
                   <button onClick={() => destroy(item.id)}>Delete</button>
                 </li>
               ))}
